ad8a34076f64c3143850db0a09d8bd4f7c61a30a
princeHrajbhar <princerajbharhariram@gmail.com>
2026-05-19 13:51:07 +0530

    readme read

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    chunkSizeWarningLimit: 1000,
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (
+            id.includes(
+              "node_modules"
+            )
+          ) {
+            if (
+              id.includes(
+                "react"
+              )
+            ) {
+              return "react";
+            }
+
+            if (
+              id.includes(
+                "react-router"
+              )
+            ) {
+              return "router";
+            }
+
+            if (
+              id.includes(
+                "lucide-react"
+              ) ||
+              id.includes(
+                "react-hot-toast"
+              )
+            ) {
+              return "ui";
+            }
+
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
